44db91ec8a5258a291e7811c9d63c9babb99ceae
Jason Ballejo <ballejo19@gmail.com>
2022-08-01 23:44:11 -0400

    commit tree e1fb2cd7c81e763870af36bf444f119e4f8dd1ef

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// let totalBatteries = 0
+// function totalBat(batteries){
+//     for(const bat of batteries){
+//         totalBatteries += bat
+//     }
+//     return totalBatteries
+// }
+
+const totalBatteries = batteryBatches.reduce(function(accumulator, element) {
+    return element += accumulator
+})
+
+// const doubledAndSummed = [1, 2, 3].reduce(function(accumulator, element){ return element * 2 + accumulator}, 0)
